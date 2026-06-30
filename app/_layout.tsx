357a2ae5abfcdd8e48e85a4cbdf7aac81b4ca804
darianaren <darianarengifo@gmail.com>
2026-06-30 15:21:20 -0300

    axios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,10 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { Text } from 'react-native';
+import { nowPlayingAction } from '@/core/actions/movies/now-playing.action';
 
 export default function RootLayout() {
+  nowPlayingAction();
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
